a3b3e39509fcd41f9023680f6c4385d7b4e91e59
wtoshiaki <88226506+wtoshiaki@users.noreply.github.com>
2021-09-14 10:33:51 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -9,4 +11,9 @@ class ApplicationController < ActionController::Base
         username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
 end
