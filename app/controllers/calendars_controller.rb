27ec3a506ed2f734325dd97748a12ea6087ee1f7
wtoshiaki <88226506+wtoshiaki@users.noreply.github.com>
2021-09-17 17:58:48 +0900

    ビューの作成

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,10 +1,46 @@
 class CalendarsController < ApplicationController
 
   def index
-
+    get_week
+    @plan = Plan.new
   end
 
   def create
+    Plan.create(plan_params)
+    redirect_to action: :index
+  end
 
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:date, :plan)
+  end
+
+  def get_week
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    @todays_date = Date.today
+
+    @week_days = []
+
+    plans = Plan.where(date: @todays_date..@todays_date + 10)
+
+    11.times do |x|
+      today_plans = []
+      plans.each do |plan|
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end
+
+      wday_num = Date.today.wday + x
+
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] }
+
+      @week_days.push(days)
+    end
   end
 end
