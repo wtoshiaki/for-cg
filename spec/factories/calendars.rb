566ee9f1a0bb36aa33e64fbedbe33a80c9f33825
wtoshiaki <88226506+wtoshiaki@users.noreply.github.com>
2021-09-20 10:49:40 +0900

    単体テストの実装

--- spec/factories/calendars.rb
+++ spec/factories/calendars.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :calendar do
-    content {Faker::Lorem.sentence}
+    plan {Faker::Lorem.sentence}
+    date {Faker::Date.between(from: '2021-09-20', to: '2021-09-27')}
     association :user
     association :room
   end
--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Calendar, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @calendar = FactoryBot.build(:calendar)
+  end
+
+  describe 'カレンダー保存' do
+    context 'カレンダーが保存できる場合' do
+      it "dateとplanの値が存在すれば保存できる" do
+        expect(@calendar).to be_valid
+      end
+    end
+
+    context 'カレンダー保存ができない場合' do
+      it 'dateが空では保存できない' do
+        @calendar.date = ''
+        @calendar.valid?
+        expect(@calendar.errors.full_messages).to include("Date can't be blank")
+      end
+
+      it 'planが空では保存できない' do
+        @calendar.plan = ''
+        @calendar.valid?
+        expect(@calendar.errors.full_messages).to include("Plan can't be blank")
+      end
+
+      it 'userが紐付いていないと保存できない' do
+        @calendar.user = nil
+        @calendar.valid?
+        expect(@calendar.errors.full_messages).to include('User must exist')
+      end
+      
+      it 'roomが紐付いていないと保存できない' do
+        @calendar.room = nil
+        @calendar.valid?
+        expect(@calendar.errors.full_messages.to include('Room must exist')
+      end
+    end
+  end
 end
+
