b0bc80e01652dde989a94b5afba177861fb10454
wtoshiaki <88226506+wtoshiaki@users.noreply.github.com>
2021-09-25 18:56:29 +0900

    エラー解消

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@example.com:wtoshiaki/for-cg.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, ’2.6.5’
+set :rbenv_ruby, '2.6.5'
 
 
 set :ssh_options, auth_methods: ['publickey'],
